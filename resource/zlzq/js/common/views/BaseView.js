a4ea658979046721bc8f21da2457f455d76fdd46
liuqiang <294007813@qq.com>
2016-01-11 09:16:34 +0800

    2.19

--- resource/zlzq/js/common/views/BaseView.js
+++ resource/zlzq/js/common/views/BaseView.js
@@ -9,7 +9,7 @@ define(['cPageView', 'cGuider', 'cUtility','Store','Model', 'UIAlert',], functio
 	var bankStore = Store.bankStore.getInstance();
 	Lizard.host="http://zlzq.easybird.cn/";
 	Lizard.apiUrl="api/v1/";
-	Lizard.version="2.18";
+	Lizard.version="2.19";
 	var viewBaseUrl="";
 	Lizard.timeout=20000;
     var BaseView = cPageView.extend({
